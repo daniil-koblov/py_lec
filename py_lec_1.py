ba53837fc0079fe933f0b5eaad201d8449012278
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-01-26 17:33:23 +0300

    third add in py_lec_1

--- py_lec_1.py
+++ py_lec_1.py
@@ -1,4 +1,45 @@
 # print(5)
 
-n = 5
-print(type(n))
+# n = 5
+# print(type(n))
+
+# m = "hghfghg"
+# print(type(m))
+
+# d = "hgh\'fghg" 'hfh"ghg"gh'
+# print(d)
+
+
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# print(a, '-', b, '-', c)
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a, b, c))
+
+
+# print('Введите первое число: ')
+# a = int(input())
+# b = int(input('Введите второе число: '))
+
+# print(a, '+', b, '=', a + b)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+
+# a = 5.89956
+# b = 6.556551
+# print(round(a*b, 3))
+
+text = 'Возмьи то не зная что и пойди не зная куда.'
+print(text[2:15])
+if text[5:6] == text[22:23]:
+    print('Да')
+else:
+    print('Нет')
