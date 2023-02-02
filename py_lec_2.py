f086b2de7afbc443ec918e4b781da8eb1ff0a433
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-02 16:43:27 +0300

    fourth add py_lec_2.py

--- py_lec_2.py
+++ py_lec_2.py
@@ -128,7 +128,7 @@
 # print(colors) # {'red', 'green', 'blue','gray'}
 # colors.remove('red')
 # print(colors) # {'green', 'blue','gray'}
-# # colors.remove('red') # KeyError: 'red'
+# colors.remove('red') # KeyError: 'red'
 # colors.discard('red') # ok
 # print(colors) # {'green', 'blue','gray'}
 # colors.clear() # { }
@@ -152,66 +152,66 @@
 
 # List Comprehension (генератор списка)
 
-# # Создать список, состоящий из четных чисел в диапазоне от 1 до 100.
+# Создать список, состоящий из четных чисел в диапазоне от 1 до 100.
 # Решение:
 
 # 1. Создать список чисел от 1 до 100
-list_1 = []
-for i in range(1, 101):
-    list_1.append(i)
-print(list_1) # [1, 2, 3,..., 100]
+# list_1 = []
+# for i in range(1, 101):
+#     list_1.append(i)
+# print(list_1) # [1, 2, 3,..., 100]
 
 # Эту же функцию можно записать так с помощью генератора списка:
-list_1 = [i for i in range(1, 101)] # [1, 2, 3,..., 100]
+# list_1 = [i for i in range(1, 101)] # [1, 2, 3,..., 100]
 
 # 2. Добавить условие (только чётные числа)
-list_1 = [i for i in range(1, 101) if i % 2 == 0]# [2, 4, 6,..., 100]
+# list_1 = [i for i in range(1, 101) if i % 2 == 0]# [2, 4, 6,..., 100]
 
 # Допустим, вы решили создать пары каждому из чисел (кортежи)
-list_1 = [(i, i) for i in range(1, 101) if i % 2 == 0]# [(2, 2), (4, 4),..., (100, 100)]
+# list_1 = [(i, i) for i in range(1, 101) if i % 2 == 0]# [(2, 2), (4, 4),..., (100, 100)]
 
 # Также можно умножать, делить, прибавлять, вычитать. Например, умножить значение на 2.
-list_1 = [i * 2 for i in range(10) if i % 2 == 0]
-print(list_1) # [0, 4, 8, 12, 16]
+# list_1 = [i * 2 for i in range(10) if i % 2 == 0]
+# print(list_1) # [0, 4, 8, 12, 16]
 
 # SyntaxError(Синтаксическая ошибка)
-number_first = 5
-number_second = 7
-if number_first > number_second # !!!!!
- print(number_first)
+# number_first = 5
+# number_second = 7
+# if number_first > number_second # !!!!!
+#  print(number_first)
 # Отсутствие :
 
 # IndentationError(Ошибка отступов)
-number_first = 5
-number_second = 7
-if number_first > number_second:
-print(number_first) # !!!!!
+# number_first = 5
+# number_second = 7
+# if number_first > number_second:
+# print(number_first) # !!!!!
 # Отсутствие отступов
 
 # TypeError(Типовая ошибка)
-text = 'Python'
-number = 5
-print(text + number)
+# text = 'Python'
+# number = 5
+# print(text + number)
 # Нельзя складывать строки и числа
 
 # ZeroDivisionError(Деление на 0)
-number_first = 5
-number_second = 0
-print(number_first // number_second)
+# number_first = 5
+# number_second = 0
+# print(number_first // number_second)
 # Делить на 0 нельзя
 
 # KeyError(Ошибка ключа)
-dictionary = {1: 'Monday', 2: 'Tuesday'}
-print(dictionary[3])
+# dictionary = {1: 'Monday', 2: 'Tuesday'}
+# print(dictionary[3])
 # Такого ключа не существует
 
 # NameError(Ошибка имени переменной)
-name = 'Ivan'
-print(names)
+# name = 'Ivan'
+# print(names)
 # Переменной names не существует
 
 #  ValueError(Ошибка значения)
-text = 'Python'
-print(int(text))
+# text = 'Python'
+# print(int(text))
 # Нельзя символы перевести в целые значения
 
