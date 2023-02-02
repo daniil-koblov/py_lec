d4a894dec851ea9247e10754311c601d7ebf4ab1
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-02 15:40:50 +0300

    second add py_lec_2.py

--- py_lec_2.py
+++ py_lec_2.py
@@ -1,6 +1,6 @@
 # Вторая лекция
 
-# list_1 = []
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # list_2 = list()
 # list_3 = [1, 2, 3, 8]
 # print(list_1)
@@ -10,8 +10,33 @@
 
 # print(list_3[0])
 # print(list_3[-1])
+# print(len(list_1)-1)
 
 # print(*list_3)
 
 # for i in list_1:
 #     print(i)
+
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1)
+# print(list_1.pop())
+# print(list_1)
+
+# a = list_1.pop()
+# print(a)
+# print(list_1.pop(3))
+# print(list_1)
+
+# print(list_1.insert(2, 14))
+# print(list_1)
+
+# print(list_1[:])
+# print(list_1[:2])
+# print(list_1[3:])
+# print(list_1[len(list_1)-2:])
+# print(list_1[2:9])
+# print(list_1[6:-18])
+# print(list_1[0:len(list_1):6])
+# print(list_1[::6])
