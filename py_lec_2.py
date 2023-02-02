3386b68bc10dbac9c8a7c48c13ee02b8fab52128
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-02 15:20:46 +0300

    fist add py_lec_2.py

--- py_lec_2.py
+++ py_lec_2.py
@@ -1,8 +1,17 @@
 # Вторая лекция
-list_1 = []
-list_2 = list()
-list_3 = [1, 2, 3, 8]
-print(list_1)
-print(list_2)
-print(list_3)
-print(*list_3)
+
+# list_1 = []
+# list_2 = list()
+# list_3 = [1, 2, 3, 8]
+# print(list_1)
+# print(list_2)
+
+# print(len(list_3))
+
+# print(list_3[0])
+# print(list_3[-1])
+
+# print(*list_3)
+
+# for i in list_1:
+#     print(i)
