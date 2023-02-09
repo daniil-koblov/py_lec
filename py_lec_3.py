4664411df9b538287cddd0abe168c3ee1a9418a9
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-09 19:01:01 +0300

    first add py_lec_3.py

---
+++ py_lec_3.py
@@ -0,0 +1,33 @@
+# def function_name(x):
+ # body line 1
+ # ...
+ # body line n
+ # optional return
+
+# def function_name(x):
+ # body line 1
+ # ...
+ # body line n
+ # optional return
+
+
+# def sum_numbes(n):
+#     summa = 0
+#     for i in range(1, n+1):
+#         summa += i
+#     return summa
+#     print('stop') # не выведется
+
+# print(sum_numbes(5))
+
+# def sum_str(*args):
+#     res = ''
+#     for i in args:
+#         res += i
+#     return res
+
+# print(sum_str('q', 'e', 'l'))
+# print(sum_str('q', 'e', 'l', 'r', 'f'))
+# print(sum_str(1, 8, 9))
+
+
