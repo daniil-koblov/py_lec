86e615f1d390f976c78f382bcc6a8460023352af
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-09 19:09:02 +0300

    first add py_lec_3_check.py

---
+++ py_lec_3_check.py
@@ -0,0 +1,15 @@
+import py_lec_3_modul
+
+print(py_lec_3_modul.max(5, 9))
+
+# или
+
+# from py_lec_3_modul import *
+
+# print(max(5, 9))
+
+# или 
+
+# import py_lec_3_modul as pl3m
+
+# print(pl3m.max(5, 9))
